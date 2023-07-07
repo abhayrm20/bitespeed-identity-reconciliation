0ab580b8965d697dd75e33e5c3ea936788593c38
Abhay Rudramuni <abhayrm20@gmail.com>
2023-07-07 15:36:43 +0530

    add: db migrations

---
+++ src/prisma/migrations/20230707100423_contact_model/migration.sql
@@ -0,0 +1,19 @@
+-- CreateEnum
+CREATE TYPE "PrecedenceTypes" AS ENUM ('primary', 'secondary');
+
+-- CreateTable
+CREATE TABLE "Contact" (
+    "id" SERIAL NOT NULL,
+    "phoneNumber" TEXT,
+    "email" TEXT,
+    "linkedId" INTEGER,
+    "linkPrecedence" "PrecedenceTypes" NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "deletedAt" TIMESTAMP(3),
+
+    CONSTRAINT "Contact_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "Contact_phoneNumber_email_idx" ON "Contact"("phoneNumber", "email");
