73c2e5f5d9c4b0fbf08d66f4d8664421f3f61486
Abhay Rudramuni <abhayrm20@gmail.com>
2023-07-09 12:49:19 +0530

    mod: contact controller

--- src/controllers/contacts-controller.ts
+++ src/controllers/contacts-controller.ts
@@ -1,27 +1,17 @@
-import { PrecedenceTypes } from '@prisma/client';
+import { Contact, PrecedenceTypes } from '@prisma/client';
 import { NotAllowedErrors } from '../configs/errorCodes';
 import { BadRequestError } from '../middlewares/errors/bad-request-error';
 import { prismaWrapper } from '../services/prisma-wrapper';
 
-const { prisma } = prismaWrapper;
-
 class Create {
-  static async newIfNotExists({
+  static async newPrimary({
     email,
     phoneNumber,
   }: {
-    email?: string;
-    phoneNumber?: string;
+    email: string | null;
+    phoneNumber: string | null;
   }) {
-    const existingContacts = await Read.byEmailOrPhone({
-      email,
-      phoneNumber,
-    });
-
-    if (existingContacts.length > 0) {
-      return existingContacts;
-    }
-
+    const { prisma } = prismaWrapper;
     return await prisma.contact.create({
       data: {
         email,
@@ -30,10 +20,31 @@ class Create {
       },
     });
   }
+
+  static async newSecondary({
+    email,
+    phoneNumber,
+    linkedId,
+  }: {
+    email: string | null;
+    phoneNumber: string | null;
+    linkedId: number;
+  }) {
+    const { prisma } = prismaWrapper;
+    return await prisma.contact.create({
+      data: {
+        email,
+        phoneNumber,
+        linkPrecedence: PrecedenceTypes.secondary,
+        linkedId,
+      },
+    });
+  }
 }
 
 class Read {
   static async byEmail(email: string) {
+    const { prisma } = prismaWrapper;
     return await prisma.contact.findMany({
       where: {
         email,
@@ -41,6 +52,7 @@ class Read {
     });
   }
   static async byPhoneNumber(phoneNumber: string) {
+    const { prisma } = prismaWrapper;
     return await prisma.contact.findMany({
       where: {
         phoneNumber,
@@ -52,8 +64,8 @@ class Read {
     email,
     phoneNumber,
   }: {
-    email?: string;
-    phoneNumber?: string;
+    email: string | null;
+    phoneNumber: string | null;
   }) {
     if (!email && !phoneNumber) {
       throw new BadRequestError(NotAllowedErrors.illegalImplementation);
@@ -64,6 +76,7 @@ class Read {
     } else if (!email && phoneNumber) {
       return await this.byPhoneNumber(phoneNumber);
     } else {
+      const { prisma } = prismaWrapper;
       return await prisma.contact.findMany({
         where: {
           OR: [
@@ -75,18 +88,229 @@ class Read {
             },
           ],
         },
+        orderBy: {
+          createdAt: 'asc',
+        },
       });
     }
   }
+
+  static async contactDetailsByPrimaryId(id: number) {
+    const { prisma } = prismaWrapper;
+
+    return await prisma.contact.findMany({
+      where: {
+        OR: [{ id }, { linkedId: id }],
+      },
+    });
+  }
 }
 
-class Update {}
+class Update {
+  static async markAsPrimary(contactId: number) {
+    const { prisma } = prismaWrapper;
+    return await prisma.contact.update({
+      where: {
+        id: contactId,
+      },
+      data: {
+        linkPrecedence: PrecedenceTypes.primary,
+        linkedId: null,
+      },
+    });
+  }
+  static async markAsSecondary(contactId: number, primaryContactId: number) {
+    const { prisma } = prismaWrapper;
+    return await prisma.contact.update({
+      where: {
+        id: contactId,
+      },
+      data: {
+        linkPrecedence: PrecedenceTypes.secondary,
+        linkedId: primaryContactId,
+      },
+    });
+  }
 
-class Delete {}
+  static async markMultipleAsSecondary(
+    contactIds: number[],
+    primaryContactId: number
+  ) {
+    const { prisma } = prismaWrapper;
+    const updatedContacts = [];
+
+    for (let contactId of contactIds) {
+      const updatedContact = await this.markAsSecondary(
+        contactId,
+        primaryContactId
+      );
+
+      updatedContacts.push(updatedContact);
+
+      // * Checking if any secondary contacts were previously linked to this contact
+      const secondaryContacts = await prisma.contact.findMany({
+        where: {
+          linkedId: contactId,
+        },
+      });
+
+      if (!secondaryContacts.length) continue;
+
+      for (let secondaryContact of secondaryContacts) {
+        const updatedSecondaryContact = await this.markAsSecondary(
+          secondaryContact.id,
+          primaryContactId
+        );
+
+        updatedContacts.push(updatedSecondaryContact);
+      }
+    }
+
+    return updatedContacts;
+  }
+}
+
+class Utils {
+  static async identify({
+    email,
+    phoneNumber,
+  }: {
+    email: string | null;
+    phoneNumber: string | null;
+  }) {
+    // * Read existing contacts
+    const existingContacts: Contact[] = await Read.byEmailOrPhone({
+      email,
+      phoneNumber,
+    });
+
+    // * If there are no contacts with same, create a new primary contact
+    if (!existingContacts.length) {
+      const contact = await Create.newPrimary({ email, phoneNumber });
+      return {
+        primaryContact: contact,
+        contacts: [contact],
+      };
+    }
+
+    /*
+     * If there are contacts with same details,
+     * 1. Same email and phone number: process existing data
+     * 2. Same email, different phone: create new secondary, process data
+     * 3. Same phone, different email: create new secondary, process data
+     *
+     * Finally check if there are multiple primaries and merge
+     */
+
+    // * finding primary contact ID
+    let primaryContactId: number;
+    const _primaryContact = existingContacts.find(
+      (contact) => contact.linkPrecedence === PrecedenceTypes.primary
+    );
+
+    console.log({ existingContacts });
+
+    if (_primaryContact) {
+      primaryContactId = _primaryContact.id;
+    } else {
+      const linkedId = existingContacts.find(
+        (contact) => !!contact.linkedId
+      )?.linkedId;
+
+      if (linkedId) {
+        primaryContactId = linkedId;
+      } else {
+        throw new BadRequestError(NotAllowedErrors.illegalImplementation);
+      }
+    }
+
+    // * Reading all contacts linked to primary contact
+    const contactsLinkedToPrimary = await Read.contactDetailsByPrimaryId(
+      primaryContactId
+    );
+
+    // * Consolidating all the contacts
+    for (let contact of contactsLinkedToPrimary) {
+      const doesExist = existingContacts.find(
+        (_contact) => _contact.id === contact.id
+      );
+
+      if (!doesExist) {
+        existingContacts.push(contact);
+      }
+    }
+
+    // * Sorting contacts
+    // ? Existing contacts was a sorted array, but sorting again in case the entries were added from previous block
+    const sortedContacts = existingContacts.sort((a, b) => {
+      return a.createdAt.getTime() - b.createdAt.getTime();
+    });
+
+    const primaryContacts = sortedContacts.filter(
+      (contact) => contact.linkPrecedence === PrecedenceTypes.primary
+    );
+
+    const primaryContact = primaryContacts[0];
+
+    // * handling multiple primary contacts
+    if (primaryContacts.length > 1) {
+      // ? Everything but the oldest one
+      const contactsToBeMarkedAsSecondary = primaryContacts.slice(
+        1,
+        primaryContacts.length
+      );
+
+      console.log({ primaryContacts, contactsToBeMarkedAsSecondary });
+
+      const updatedContacts = await Update.markMultipleAsSecondary(
+        contactsToBeMarkedAsSecondary.map((contact) => contact.id),
+        primaryContact.id
+      );
+
+      for (let contact of updatedContacts) {
+        const index = sortedContacts.findIndex(
+          (_contact) => _contact.id === contact.id
+        );
+
+        // * Replacing updated contacts in the sorted contacts list
+        sortedContacts.splice(index, 1, contact);
+      }
+    }
+
+    // * Check if there's a contact with same details
+    const contactWithSameDetails = sortedContacts.find(
+      (contact) =>
+        contact.phoneNumber === phoneNumber && contact.email === email
+    );
+
+    console.log({
+      existingContacts,
+      sortedContacts,
+      primaryContacts,
+      primaryContact,
+    });
+
+    // * If there's no contact with same details, creating a new secondary contact
+    if (!contactWithSameDetails) {
+      const newContact = await Create.newSecondary({
+        email,
+        phoneNumber,
+        linkedId: primaryContact.id,
+      });
+
+      sortedContacts.push(newContact);
+    }
+
+    return {
+      primaryContact,
+      contacts: sortedContacts,
+    };
+  }
+}
 
 export default {
   Create,
   Read,
   Update,
-  Delete,
+  Utils,
 };
